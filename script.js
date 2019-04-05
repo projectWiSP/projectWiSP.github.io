24d0565ba0fdac4ab2eb608c98124ed628487534
jackcrane <45675751+jackcrane@users.noreply.github.com>
2019-04-04 21:34:38 -0400

    First legit push

---
+++ script.js
@@ -0,0 +1,37 @@
+const SLIDES_MAP = {
+	0: "WiSP_conceptLogo.jpg",
+	1: "1.png",
+	2: "2.jpg",
+	3: "3.png",
+	4: "4.png",
+	5: "5.png",
+}
+function numberToSrc(number){
+	console.log(SLIDES_MAP[number])
+	document.getElementById("slideshow").src = SLIDES_MAP[number]
+}
+var currentSlide = 0
+
+function lastslide() {
+	if(currentSlide!=0) {
+		currentSlide -= 1
+		numberToSrc(currentSlide)
+		console.log(currentSlide)
+	} else {
+		currentSlide = 5
+		numberToSrc(currentSlide)
+		console.log(currentSlide)
+	}
+}
+
+function nextslide() {
+	if(currentSlide!=5) {
+		currentSlide += 1
+		numberToSrc(currentSlide)
+		console.log(currentSlide)
+	} else {
+		currentSlide = 0
+		numberToSrc(currentSlide)
+		console.log(currentSlide)
+	}
+}
